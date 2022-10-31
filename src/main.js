1f1b8c3892dbab08a3ae9372265c68263c30cc39
fengtianxi <625362207@qq.com>
2022-10-31 18:42:39 +0800

    empty: empty branch

--- src/main.js
+++
@@ -1,5 +0,0 @@
-import ThreeBase from "./ThreeBase";
-
-const container = document.getElementById("app");
-const threeBase = new ThreeBase(container);
-// threeBase.initSphere();
--- src/utils.js
+++
@@ -1,5 +0,0 @@
-export function fetchImage(name) {
-    const path = `./images/${name}.jpg`;
-    const modules = import.meta.globEager("./images/*.jpg");
-    return modules[path].default;
-  }
